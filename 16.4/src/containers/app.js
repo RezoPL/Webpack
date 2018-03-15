8c2b12dfd6057419852f5a82ed21f3d68ccc4d6d
Przemek Jurkiewicz <przem.jurkiewicz85@gmail.com>
2018-03-15 18:38:50 +0100

    app.js formatting

--- 16.4/src/containers/app.js
+++ 16.4/src/containers/app.js
@@ -3,33 +3,34 @@ import style from './App.css';
 import Title from '../components/title.js';
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             data: []
         };
     }
-    addTodo(val){
+    addTodo(val) {
         const todo = {
             text: val,
             id: uuid.v4(),
         };
         const data = [...this.state.data, todo];
-        this.setState({data});
+        this.setState({ data });
     }
 
     removeTodo(id) {
-    const remainder = this.state.data.filter(todo => todo.id !== id);
-    this.setState({data: remainder});
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({ data: remainder });
     }
 
     render() {
-        return (
-            <div className={style.TodoApp}>
-                <Title title = 'To do list'
-                    count = {this.state.data.length}
-                />
-            </div>
+        return ( <
+            div className = { style.TodoApp } >
+            <
+            Title title = 'To do list'
+            count = { this.state.data.length }
+            /> <
+            /div>
         );
     }
 }
